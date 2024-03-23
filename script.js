43f23c71e826f2a543ca2d2408b58587f0e46b5f
RB4eva <sibemeyer@web.de>
2024-03-23 21:05:57 +0100

    Reset

--- script.js
+++ script.js
@@ -24,22 +24,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// Zum Beispiel für ein Dropdown-Menü bei kleineren Bildschirmgrößen
-document.addEventListener("DOMContentLoaded", function() {
-  const navbar = document.getElementById("navbar");
-  const links = navbar.querySelectorAll("a");
-
-  links.forEach(function(link) {
-    link.addEventListener("click", function(event) {
-      event.preventDefault(); // Prevent default link behavior
-      const targetId = this.getAttribute("href").substr(1); // Get the target ID from href attribute
-      const targetElement = document.getElementById(targetId); // Find the target element by ID
-      if (targetElement) {
-        // Smooth scroll to the target element
-        targetElement.scrollIntoView({
-          behavior: "smooth" // Smooth scroll behavior
-        });
-      }
-    });
-  });
-});
